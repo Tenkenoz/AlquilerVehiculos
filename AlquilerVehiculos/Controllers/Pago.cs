5bf5c1172db6d53f2ebdeabeb22a28eb3129506a
Tenkenoz <epobando1@espe.edu.ec>
2025-03-01 22:24:39 -0500

    Se realizo las demas pestañas del panel administrativo como empleados, pago, seguros y vehiculo. Teniendo cada uno un modal que permita guardar sus datos.

---
+++ AlquilerVehiculos/Controllers/Pago.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlquilerVehiculos.Controllers
+{
+    public class Pago : Controller
+    {
+        // GET: Pago
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Pago/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Pago/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pago/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pago/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Pago/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pago/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Pago/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
